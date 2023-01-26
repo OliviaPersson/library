1e43f376b0437957c55dbc3c9cdb1b1200769e81
Olivia Persson <olivia.persson@forefront.se>
2023-01-26 10:47:45 +0100

    Add json test data and temporary images

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,6 +1,5 @@
 import "./MainContent.css";
 import theWish from "../images/the-wish.png";
-import enEndaOnskan from "../images/en-enda-onskan.png";
 
 function MainContent() {
   return (
@@ -12,27 +11,6 @@ function MainContent() {
           <div className="book-data">
             <p className="title">Things Fall Apart</p>
             <p className="author">Chinua Achebe</p>
-          </div>
-        </div>
-        <div className="card-container">
-          <img src={enEndaOnskan} />
-          <div className="book-data">
-            <p className="title">Fairy tales</p>
-            <p className="author">Hans Christian Andersen</p>
-          </div>
-        </div>
-        <div className="card-container">
-          <img src={theWish} />
-          <div className="book-data">
-            <p className="title">Things Fall Apart</p>
-            <p className="author">Chinua Achebe</p>
-          </div>
-        </div>
-        <div className="card-container">
-          <img src={enEndaOnskan} />
-          <div className="book-data">
-            <p className="title">Fairy tales</p>
-            <p className="author">Hans Christian Andersen</p>
           </div>
         </div>
       </div>
